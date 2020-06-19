10f4dfe51740a20bc4970c2a31899dc899cfa87c
Jono Cosio <jonoluiz@gmail.com>
2020-06-19 22:34:38 +0200

    Add comments to Handler class

--- src/classes/command-handler.js
+++ src/classes/command-handler.js
@@ -1,15 +1,25 @@
 module.exports = class Handler {
     
+    /**
+     * Set prefix to be used by Handler
+     * @param {String} prefix 
+     */
     constructor(prefix) {
         // Set prefix to be bot prefix
         this.prefix = prefix;
     }
 
+    /**
+     * Main handler method
+     * @param {Message} msg 
+     */
     handleCommands = msg => {
         // Check message if it uses the prefix or sent by bot
         if (!msg.content.startsWith(this.prefix) || msg.author.bot) return;
 
+        //Take arguments from message
         const args = msg.content.slice(this.prefix.length).split(/ +/);
+        //First arg after prefix is the command
         const command = args.shift().toLowerCase();
 
         // Main switch block
@@ -24,7 +34,14 @@ module.exports = class Handler {
                 break;
         }
     }
+
     // Maybe put the commands in another class?
+
+    /**
+     * Basic sample command
+     * @param {Message} msg 
+     * @param {Array} args 
+     */
     ping = (msg, args) => {
         msg.channel.send('Pong');
     }
