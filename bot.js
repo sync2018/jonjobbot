97cb785de4eac80788f918bbb892d5f55ee8e4bf
Jono Cosio <jonoluiz@gmail.com>
2020-06-19 22:25:05 +0200

    Use Handler class for handling commands

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
+// Imports
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const Handler = require('./src/classes/command-handler');
+
 const triggerKey = ";";
-const server = client.guilds.cache.get(serverID);
+// const server = client.guilds.cache.get(serverID);
+const handler = new Handler(triggerKey);
 
-
-client.on('ready', () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === triggerKey+'ping') {
-    msg.reply('pong');
-  }
+  // Let Handler take care of commands
+  handler.handleCommands(msg);
 });
 
 client.on('message', msg => {
